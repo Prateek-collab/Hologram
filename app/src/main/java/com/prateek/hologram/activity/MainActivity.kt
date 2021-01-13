a1769a0d26bf40defe09107e347f0ef618c4ce92
Prateek Ranjan <60317855+Prateek-collab@users.noreply.github.com>
2021-01-13 22:14:21 +0530

    Fragments initialized

--- app/src/main/java/com/prateek/hologram/activity/MainActivity.kt
+++ app/src/main/java/com/prateek/hologram/activity/MainActivity.kt
@@ -4,34 +4,43 @@ import android.os.Bundle
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.prateek.hologram.R
+import com.prateek.hologram.fragment.HomeFragment
+import com.prateek.hologram.fragment.NotificationsFragment
+import com.prateek.hologram.fragment.ProfileFragment
+import com.prateek.hologram.fragment.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView:TextView
+    internal lateinit var selectedFragment:Fragment
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                textView.setText("Home")
-               return@OnNavigationItemSelectedListener true
+                selectedFragment = HomeFragment()
             }
             R.id.nav_search -> {
-                textView.setText("Search")
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = SearchFragment()
             }
             R.id.nav_upload -> {
                 textView.setText("Upload")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
-                textView.setText("Notifications")
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = NotificationsFragment()
             }
             R.id.nav_profile -> {
-                textView.setText("Profile")
-               return@OnNavigationItemSelectedListener true
+                selectedFragment = ProfileFragment()
             }
+        }
+        if (selectedFragment!=null)
+        {
+            supportFragmentManager.beginTransaction().replace(
+                    R.id.frame_layout,
+                    selectedFragment!!
+            ).commit()
         }
 
         false
@@ -45,5 +54,10 @@ class MainActivity : AppCompatActivity() {
         textView=findViewById(R.id.message)
 
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(
+                R.id.frame_layout,
+                HomeFragment()
+        ).commit()
     }
 }
